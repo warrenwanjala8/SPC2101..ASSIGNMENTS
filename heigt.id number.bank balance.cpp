fcc7f0f12ecb9e9eeb01143339392dd47aa1939d
warrenwanjala8 <warrenwanjala8@gmail.com>
2025-09-23 11:39:18 +0300

    Add files via upload

---
+++ heigt.id number.bank balance.cpp
@@ -0,0 +1,32 @@
+//Program to display;Height,ID Number,and Bank Balance
+/*
+NAME:WANJALA SEITH WARREN
+REG NO:PA106/G/29294/25
+DESCRIPTION:Program to display my height,ID number and Bank balance
+*/
+#include <stdio.h> 
+int main(){
+	int height;
+	int ID_Number;
+	int bank_balance;
+	int phone_number;
+	
+	printf("Enter your height;");
+	scanf("%d",&height);
+	printf("your height is %d\n",height);
+	
+	printf("Enter your id_number:");
+	scanf("%d",&ID_Number);
+	printf("your ID Number is %d\n",ID_Number);
+	
+	printf("Enter your bank balance:");
+	scanf("%d",&bank_balance);
+	printf("your bank_balance is %d\n",bank_balance);
+	
+	printf("Enter your phone number:");
+	scanf("%d",&phone_number);
+	printf("your phone number is %d",phone_number);
+	
+	return 0;
+}
+	
